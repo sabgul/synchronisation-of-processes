42da55078189323a38c7301bee1047fc45f601a1
sabinathe <41567711+sabgul@users.noreply.github.com>
2020-04-16 17:40:32 +0200

    Modify structure

--- proj2.c
+++ proj2.c
@@ -16,7 +16,9 @@
 #include <semaphore.h>
 #include <unistd.h>
 #include <sys/types.h>
-
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <pthread.h>
 /*--------------------Constants--------------------*/
 #define FAIL 1
 #define SUCCESS 0
@@ -27,34 +29,25 @@
 /*----------------Global variables-----------------*/
 sem_t *semaphore = NULL;
 FILE *output;
-
-
+int action = 0;
 
 /*-------------------Functions--------------------*/
 int errorCheck(int argc, char *argv[]);
 int initialise();
-
+void cleanup();
+void open_semaphores();
+void close_semaphores();
 /*------------------Semaphores--------------------*/
 
-
+sem_t *printMessage;
+sem_t *allRegistered;
+sem_t *judgeInHouse;
+sem_t *ongoingConfirmation;
 
 //deklarovat semafor
 //namapovat do pamate
 //pouzit
 //nakoniec vsetky uzavriet
-
-//chyby vypisovat na stderr, exit code 1
-//tiez uvolnit vsetky alokovane zdroje
-
-//xgulci00.ios.proj2.nazov
-
-/**
-* immigrants == PI
-* timeImm == IG
-* timeJudgeEnter == JG
-* timeGetCertificate == IT
-* timeIssueCertificate == JT
-**/
 
 /*-------------------Functions--------------------*/
 
@@ -102,27 +95,42 @@ int errorCheck(int argc, char *argv[]) {
 }
 
 int initialise() {
-  output = fopen("proj2.out", w);
-  if(semaphore = sem_open("/xgulci00.ios.proj2.semaphore", O_CREAT | O_EXCL, 0666, 1)== SEM_FAILED) {
-    fprintf(stderr, "error: initialisation of semaphore failed\n", );
-    return FAIL;
-  }
+  output = fopen("proj2.out", "w");
+  // if(semaphore = sem_open("/xgulci00.ios.proj2.semaphore", O_CREAT | O_EXCL, 0666, 1)== SEM_FAILED) {
+  //   fprintf(stderr, "error: initialisation of semaphore failed\n");
+  //   return FAIL;
+  // }
+  open_semaphores();
   return SUCCESS;
 }
 
+void open_semaphores() {
+  printMessage = sem_open("/xgulci00.ios.proj2.print", O_CREAT | O_EXCL, 0644, 1);
+  //judgeInHouse = sem_open("/xgulci00.ios.proj2.judge", O_CREAT | O_EXCL, 0644, 0);
+  //allRegistered = sem_open("/xgulci00.ios.proj2.registration", O_CREAT | O_EXCL, 0644, 0);
+  //ongoingConfirmation = sem_open("/xgulci00.ios.proj2.confirmation", O_CREAT | O_EXCL, 0644, 0);
+}
+
+void close_semaphores() {
+  sem_unlink("/xgulci00.ios.proj2.print");
+  // sem_unlink("/xgulci00.ios.proj2.judge");
+  // sem_unlink("/xgulci00.ios.proj2.registration");
+  // sem_unlink("/xgulci00.ios.proj2.confirmation");
+}
+
 void cleanup() {
+  close_semaphores();
   //sem_close(sem);
-  sem_unlink("xgulci00.ios.proj2.semaphore");
   if (output != NULL) {
     fclose(output);
   }
 }
-//
-// void process_judge(delay,) {
+
+// void process_judge(delay, timeToEnter, timeToConfirm) {
 //   exit(0);
 // }
 //
-// void process_immigrant(delay, ) {
+// void process_immigrant(delay, numberOfImmigrants, timeToGetCertif) {
 //   exit(0);
 // }
   //
@@ -138,57 +146,52 @@ int main(int argc, char *argv[]) {
   if (errorCheck(argc, argv) == FAIL)
     return FAIL;
 
-  if (initialise() == FAIL) {
-    cleanup();
-    return FAIL;
-  }
-    // pid_t judge = fork();
-    //
-    // if(judge < 0) {
-    //   fprintf(stderr, "error: invalid fork\n");
-    //   return 1;
-    // } else if (judge == 0) {
-    //   //judge
-    // } else {
-    //   //gen_immigrants(pocet imigrantov, delay);
-    // }
-    //
-    // pid_t judge = fork();
-    //
-    // if (first < 0) {
-    //   fprintf(stdout, "Fork didnt work\n");
-    // } else if (first == 0) {
-    //   fprintf(stdout, "Ahoj ja som child process 1\n");
-    // } else {
-    //   fprintf(stdout, "Ahoj ja som parent process 1\n");
-    //   pid_t second = fork();
-    //   if(second < 0) {
-    //     fprintf(stdout, "Chyba v druhom forku\n");
-    //   } else if (second == 0) {
-    //     fprintf(stdout, "AHoj ja som child process 2\n");
-    //   } else {
-    //     fprintf(stdout, "Ahoj ja som co ostalo\n");
-    //   }
-    // }
-    //
-    //prvni fork
-    // pid_t immigrant = fork();
-    //
-    // if (immigrant < 0) {
-    //     fprintf(stderr,"Vytvoreni child procesu selhalo(hacker)\n");
-    //     // cleanUp(id,ptr);
-    //     // exit(1);
-    //     return 1;
-    // }
-    // else if (hacker == 0) {
-    //     //child process #1
-    //
-    //     // processHack(hackTime,People,ptr,maxWaitTime,capacity,sailTime);
-    // }
-    // else
-    // {//parent
+    if (initialise() == FAIL) {
+      cleanup();
+      return FAIL;
+    }
 
+/*----------------------redo-----------------------*/
+char *end;
+//number of immigrant processes to be generated
+int PI = strtol(argv[1], &end, 10);
+//max time for generation of new process
+int IG = strtol(argv[2], &end, 10);
+//max time for judges entrance since he last left
+int JG = strtol(argv[3], &end, 10);
+//max time for obtaining of certificate
+int IT = strtol(argv[4], &end, 10);
+//max time for issuance of a certificate
+int JT = strtol(argv[5], &end, 10);
+/*------------------------------------------------*/
+
+//First division of process
+    pid_t judge = fork();
+
+    if (judge < 0) {
+      fprintf(stderr, "error: fork failed\n");
+      return FAIL;
+    } else if (judge == 0) {
+      //judge child process
+      //process_judge();
+      //exit(0);
+    } else {
+      //second division of process
+      pid_t immigrant = fork();
+
+      if (immigrant < 0) {
+        fprintf(stderr, "error: fork failed\n");
+        return FAIL;
+      } else if (immigrant == 0) {
+        //immgirant child process
+        //gen_immigrants();
+        //exit(0);
+      } else {
+        //waitpid();
+      }
+      //waitpid();
+    }
   cleanup();
-  exit(0);
+  //exit(0);
   return SUCCESS;
 }
